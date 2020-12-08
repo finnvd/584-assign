927bd83b71580feb1def28912476345bc76481ed
finnvd <62909724+finnvd@users.noreply.github.com>
2020-12-07 22:02:18 -0800

    Add files via upload

---
+++ HW5_584_Prob2.m
@@ -0,0 +1,30 @@
+clc;
+close all;
+clearvars;
+
+imds=imageDatastore('C:\Users\Finn\Documents\MATLAB\584\CroppedYale','FileExtensions',{'.pgm'},'IncludeSubfolders',1);
+
+A=[];
+imgcol=zeros(32256,1);
+for i=1:120
+    img=readimage(imds,i);
+    for j=1:32256
+        imgcol(j,1)=img(j);
+    end
+    A=[A imgcol];
+end
+
+B=A.';
+C=A*B;
+
+v=randn(32256,1);   %Initial vector guess
+iter=20;        %Max number of iterations
+
+for i=1:iter
+    Cv=C*v;
+    lam=v'*Cv;
+    v=Cv/norm(Cv);
+end
+
+v=Cv/lam;
+
